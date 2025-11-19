1965ec95c2f094c90c7f77374355cb872e9a54af
Rustaman254 <ofury47@gmail.com>
2025-11-19 19:30:14 +0300

    compact removed

--- dot-ramp/eslint.config.mjs
+++ dot-ramp/eslint.config.mjs
@@ -7,9 +7,6 @@ export default [
   {
     ignores: ['app/descriptors/**'],
   },
-  ...compat.config({
-    extends: ['next/core-web-vitals', 'next/typescript'],
-  }),
   {
     rules: {
       '@typescript-eslint/no-unused-vars': 'off',
